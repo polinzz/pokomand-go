e98f1b262a123c2d61ecf3c36777707065368858
lightvic <vguillerd@gmail.com>
2024-01-19 11:13:06 +0100

    add call db in go code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module pokomand-go
 
 go 1.21
+
+require github.com/go-sql-driver/mysql v1.7.1
--- main/main.go
+++ main/main.go
@@ -1,10 +1,37 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
 	"net/http"
+	"time"
 )
 
 func main() {
+	time.Sleep(10 * time.Second)
+	conf := mysql.Config{
+		User:                 "root",
+		Passwd:               "password",
+		Net:                  "tcp",
+		Addr:                 "database:5657",
+		DBName:               "gobase",
+		AllowNativePasswords: true,
+	}
+
+	db, err := sql.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		http.HandleFunc("/erreur1", func(writer http.ResponseWriter, request *http.Request) {
+			writer.Write([]byte("Erreur 1 !"))
+		})
+	}
+
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		http.HandleFunc("/erreur2", func(writer http.ResponseWriter, request *http.Request) {
+			writer.Write([]byte("Erreur 2 !"))
+		})
+	}
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("Hello world !"))
 	})
